cfaae75cb8d6881788117f42f474d57bbebb4602
Alex Gyujin Kim <agk2144@columbia.edu>
2018-10-30 15:10:47 -0400

    user accounts all work

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import login, authenticate
-from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
 from django.views import View
 from .forms import SignupForm
@@ -11,14 +10,17 @@ class Signup(View):
     template_name = 'registration/register.html'
 
     def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
+        form = self.form_class(request.POST or None)
         if form.is_valid():
             form.save()
+
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
+
             return redirect('hike_list')
+        return render(request, self.template_name, {'form': form})
 
     def get(self, request, *args, **kwargs):
         form = SignupForm(initial=self.initial)
