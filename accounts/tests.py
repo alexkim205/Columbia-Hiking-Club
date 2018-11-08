806b65c306bdee21662efe7225091816f19b1103
Alex Gyujin Kim <alexkim205@yahoo.com>
2018-11-08 02:13:10 -0500

    added auth rest tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -3,7 +3,9 @@ from django.urls import reverse
 
 from rest_framework.test import APITestCase
 from rest_framework import status
-from rest_framework.authtoken.models import Token
+# from rest_framework.authtoken.models import Token
+
+from knox.auth import AuthToken
 
 
 # Create your tests here.
@@ -20,8 +22,10 @@ class AccountsTest(APITestCase):
         self.model = get_user_model()
         self.test_user = self.model.objects.create_user(**data)
         self.create_url = reverse('auth_register')
+        self.login_url = reverse('auth_login')
+        self.current_url = reverse('current_user_api')
 
-    def test_create_user(self):
+    def test_create_user_returns_serialized_token(self):
         """
         Ensure we can create a new user and a valid token is created with it.
         """
@@ -40,14 +44,14 @@ class AccountsTest(APITestCase):
         # And that we're returning a 201 created code.
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         # Additionally, we want to return the username and email upon successful creation.
-        self.assertEqual(response.data['first_name'], data['first_name'])
-        self.assertEqual(response.data['last_name'], data['last_name'])
-        self.assertEqual(response.data['email'], data['email'])
-        self.assertFalse('password' in response.data)
+        self.assertEqual(response.data['user']['first_name'], data['first_name'])
+        self.assertEqual(response.data['user']['last_name'], data['last_name'])
+        self.assertEqual(response.data['user']['email'], data['email'])
+        self.assertIn('password', response.data['user'])
+        # self.assertFalse('password' in response.data)
 
-        # Check if token matches up with user
-        token = Token.objects.get(user=user)
-        self.assertEqual(response.data['token'], token.key)
+        # Check if token is returned
+        self.assertIn('token', response.data)
 
     def test_create_user_with_short_password(self):
         """
@@ -90,3 +94,49 @@ class AccountsTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(self.model.objects.count(), 1)
         self.assertEqual(len(response.data['email']), 1)
+
+    def test_login_user(self):
+        auth_data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'johndoe@email.com',
+            'password': 'johnspassword'
+        }
+        login_data = {
+            'email': 'johndoe@email.com',
+            'password': 'johnspassword'
+        }
+
+        response = self.client.post(self.login_url, login_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        self.assertEqual(response.data['user']['first_name'], auth_data['first_name'])
+        self.assertEqual(response.data['user']['last_name'], auth_data['last_name'])
+        self.assertEqual(response.data['user']['email'], auth_data['email'])
+        self.assertIn('password', response.data['user'])
+
+    def test_get_current_user(self):
+        auth_data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'johndoe@email.com',
+            'password': 'johnspassword'
+        }
+        login_data = {
+            'email': 'johndoe@email.com',
+            'password': 'johnspassword'
+        }
+
+        # First login to get Auth Token
+        auth_response = self.client.post(self.login_url, login_data, format='json')
+        self.assertEqual(auth_response.status_code, status.HTTP_201_CREATED)
+        token = auth_response.data['token']
+
+        # Use Auth Token to check current user
+        self.client.credentials(HTTP_AUTHORIZATION=('Token %s' % token))
+        current_response = self.client.get(self.current_url, format='json',)
+
+        self.assertEqual(current_response.data['first_name'], auth_data['first_name'])
+        self.assertEqual(current_response.data['last_name'], auth_data['last_name'])
+        self.assertEqual(current_response.data['email'], auth_data['email'])
+        self.assertIn('password', current_response.data)
